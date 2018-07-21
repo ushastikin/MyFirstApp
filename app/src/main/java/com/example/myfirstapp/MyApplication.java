2f95cd44bb52614c063942df42337b0e2e642929
Ilya Paskhover <ilyapaskhover@gmail.com>
2018-07-21 13:45:42 +0300

    Update Fabric token to publish a version with integrated Bugsee

--- app/src/main/java/com/example/myfirstapp/MyApplication.java
+++ app/src/main/java/com/example/myfirstapp/MyApplication.java
@@ -8,6 +8,6 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        Bugsee.launch(this, "fdc9e6c4-be19-494b-b059-c37d19283054");
+        Bugsee.launch(this, "71c1937d-da06-4a94-8b4d-52d08b515f96");
     }
 }
