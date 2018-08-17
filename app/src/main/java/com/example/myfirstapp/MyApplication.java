fa86e55bd5d1811a8a881c7bfd549e9be56a47d9
Ilya Paskhover <ilyapaskhover@gmail.com>
2018-08-17 23:40:48 +0300

    Add AppSpector https://appspector.com/ - update SDK and gradle for this

--- app/src/main/java/com/example/myfirstapp/MyApplication.java
+++ app/src/main/java/com/example/myfirstapp/MyApplication.java
@@ -3,11 +3,31 @@ package com.example.myfirstapp;
 import android.app.Application;
 
 import com.bugsee.library.Bugsee;
+import com.appspector.sdk.AppSpector;
 
 public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
         Bugsee.launch(this, "71c1937d-da06-4a94-8b4d-52d08b515f96");
+
+        // We recommend to start AppSpector from Application#onCreate method
+
+        // You can start all monitors
+        AppSpector
+                .build(this)
+                .withDefaultMonitors()
+                .run("OGJjOTE4YTQtMzhjMi00YTRiLThkMGQtNDc5YTcxMzM1NGE0");
+
+//        // Or you can select monitors that you want to use
+//        AppSpector
+//                .build(this)
+//                .addPerformanceMonitor()
+//                .addHttpMonitor()
+//                // If specific monitor is not added then this kind of data won't be tracked and available on the web
+//                .addLogMonitor()
+//                .addScreenshotMonitor()
+//                .addSQLMonitor()
+//                .run("OGJjOTE4YTQtMzhjMi00YTRiLThkMGQtNDc5YTcxMzM1NGE0");
     }
 }
